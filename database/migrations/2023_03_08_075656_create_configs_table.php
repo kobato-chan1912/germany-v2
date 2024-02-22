da683c00cc929245e193527c7f4e46812bd6e526
kobato-chan1912 <no_body156@yahoo.com.vn>
2024-02-23 02:00:17 +0700

    update

--- database/migrations/2023_03_08_075656_create_configs_table.php
+++ database/migrations/2023_03_08_075656_create_configs_table.php
@@ -13,22 +13,22 @@ class CreateConfigsTable extends Migration
      */
     public function up()
     {
-        Schema::create('configs', function (Blueprint $table) {
-            $table->id();
-            $table->string('config_name');
-            $table->string("config_value", 2500)->nullable();
-            $table->timestamps();
-        });
-        \App\Models\Config::insert([
-            ["config_name" => "home_title"],
-            ["config_name" => "home_description"],
-            ["config_name" => "youtube"],
-            ["config_name" => "facebook"],
-            ["config_name" => "twitter"],
-            ["config_name" => "pinterest"],
-            ["config_name" => "telegram"],
-            ["config_name" => "tiktok"],
-        ]);
+//        Schema::create('configs', function (Blueprint $table) {
+//            $table->id();
+//            $table->string('config_name');
+//            $table->string("config_value", 2500)->nullable();
+//            $table->timestamps();
+//        });
+//        \App\Models\Config::insert([
+//            ["config_name" => "home_title"],
+//            ["config_name" => "home_description"],
+//            ["config_name" => "youtube"],
+//            ["config_name" => "facebook"],
+//            ["config_name" => "twitter"],
+//            ["config_name" => "pinterest"],
+//            ["config_name" => "telegram"],
+//            ["config_name" => "tiktok"],
+//        ]);
     }
 
     /**
